0633b1480a1d6c41f01404522a383145af5595bb
Corey Nance <coreynance83@gmail.com>
2021-07-19 15:02:20 -0400

    completed changes to homeroutes and blogpost handlebars file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,7 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
+ 
     const blogPostData = await BlogPost.findAll({
       include: [
         {
@@ -14,11 +14,10 @@ router.get('/', async (req, res) => {
       ],
     });
 
-    // Serialize data so the template can read it
+  
     const blogPosts = blogPostData.map((blogPost) => blogPost.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
-    // res.render('homepage');
+
     res.render('homepage', { 
       blogPosts, 
       logged_in: req.session.logged_in 
@@ -65,10 +64,10 @@ router.get('/blogPost/:id', withAuth, async (req, res) => {
   }
 });
 
-// Use withAuth middleware to prevent access to route
+
 router.get('/profile', withAuth, async (req, res) => {
   try {
-    // Find the logged in user based on the session ID
+    
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
       include: [{ model: BlogPost }],
@@ -86,7 +85,7 @@ router.get('/profile', withAuth, async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
+
   if (req.session.logged_in) {
     res.redirect('/profile');
     return;
@@ -96,7 +95,7 @@ router.get('/login', (req, res) => {
 });
 
 router.get('/signup', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
+ 
   if (req.session.logged_in) {
     res.redirect('/profile');
     return;
